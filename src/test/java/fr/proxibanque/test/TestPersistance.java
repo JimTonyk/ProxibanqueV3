73366833088be83352f424496e07b2c2f22833cc
Jean-Michel Hiltbrunner <jm.hiltbrunner@outlook.com>
2018-04-18 17:37:38 +0200

    Création du projet Proxibanque V3

---
+++ src/test/java/fr/proxibanque/test/TestPersistance.java
@@ -0,0 +1,15 @@
+package fr.proxibanque.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class TestPersistance {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
