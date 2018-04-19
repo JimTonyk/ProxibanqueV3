effe0035ef91fe824bc9aa6f897cda48782d0843
Jean-Michel Hiltbrunner <jm.hiltbrunner@outlook.com>
2018-04-19 14:32:11 +0200

    Ajout du Rest Client

---
+++ src/main/webapp/restClient.js
@@ -0,0 +1,64 @@
+function doGet(){
+	$.ajax({
+    	type: "GET",
+    	 contentType: "application/json; charset=utf-8",
+         dataType: "json",
+        url: "http://localhost:8080/proxibanque/services/clientservice/clients/"+$('#clientID').val()
+    }).then(function(data) {
+       $('#clientID').val(data.id);
+       $('#clientNom').val(data.nom);
+       $('#clientPrenom').val(data.prenom)
+       $('#clientAdresse').val(data.adresse);
+       $('#clientCodePostal').val(data.codepostal);
+       $('#clientVille').val(data.ville);
+       $('#clientTelephone').val(data.telephone);
+    });
+}
+	
+function doPost(){
+$.ajax({
+	type: "POST",
+	 contentType: "application/json; charset=utf-8",
+     dataType: "json",
+     data: JSON.stringify({name:$('#clientName').val()}),
+    url: "http://localhost:8080/proxibanque/services/clientservice/clients"
+    	
+	}).then(function(data) {
+		$('#clientID').val(data.id);
+		$('#clientName').val(data.nom);
+		$('#clientPrenom').val(data.prenom)
+	    $('#clientAdresse').val(data.adresse);
+	    $('#clientCodePostal').val(data.codepostal);
+	    $('#clientVille').val(data.ville);
+	    $('#clientTelephone').val(data.telephone);
+	});
+    
+}
+
+function doPut(){
+	$.ajax({
+    	type: "PUT",
+    	 contentType: "application/json; charset=utf-8",
+         dataType: "json",
+        url: "http://localhost:8080/proxibanque/services/clientservice/clients/"
+    }).then(function(data) {
+    	$('#clientID').val(data.id);
+        $('#clientNom').val(data.nom);
+        $('#clientPrenom').val(data.prenom)
+        $('#clientAdresse').val(data.adresse);
+        $('#clientCodePostal').val(data.codepostal);
+        $('#clientVille').val(data.ville);
+        $('#clientTelephone').val(data.telephone);
+    });
+}
+	
+function doDelete(){
+	$.ajax({
+    	type: "DELETE",
+    	 contentType: "application/json; charset=utf-8",
+         dataType: "json",
+        url: "http://localhost:8080/proxibanque/services/clientservice/clients/"+$('#clienttID').val()
+    }).then(function(data) {
+       alert('Client supprimé de Proxibanque');
+    });
+}
--- src/test/java/fr/proxibanque/test/RestClient.java
+++ src/test/java/fr/proxibanque/test/RestClient.java
@@ -11,26 +11,26 @@ public class RestClient {
 	private static final String PATIENTS = "/Clients";
 	private static final String SERVICE_PATIENTS = "http://localhost:8080/Proxibanque/services/clientservice";
 
-//	public static void main(String[] args) {
-//		Client client = ClientBuilder.newClient();
-//		// Cible
-//		WebTarget target = client.target(SERVICE_PATIENTS).path(PATIENTS).path("/1000");
-//		WebTarget putTarget = client.target(SERVICE_PATIENTS).path(PATIENTS);
-//		// Verbe
-//		Response response = target.request().get();
-//		((fr.proxibanque.model.Client) p) = response.readEntity(Client.class);
-//		// Afficher Get
-//		System.out.println(p.getNom() + " status " + response.getStatus());
-//
-//		// Méthode put
-//		(fr.proxibanque.model.Client) c = new Client();
-//		((fr.proxibanque.model.Client) c).setNom("John");
-//		((fr.proxibanque.model.Client) c).setIdClient(1001);
-//		Response putResponse = putTarget.request().put(Entity.entity(c, MediaType.APPLICATION_XML));
-//
-//		System.out.println("put =" + putResponse.getStatus());
-//		client.close();
-//		response.close();
-//
-//	}
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		// Cible
+		WebTarget target = client.target(SERVICE_PATIENTS).path(PATIENTS).path("/1000");
+		WebTarget putTarget = client.target(SERVICE_PATIENTS).path(PATIENTS);
+		// Verbe
+		Response response = target.request().get();
+		((fr.proxibanque.model.Client) p) = response.readEntity(Client.class);
+		// Afficher Get
+		System.out.println(p.getNom() + " status " + response.getStatus());
+
+		// Méthode put
+		(fr.proxibanque.model.Client) c = new Client();
+		((fr.proxibanque.model.Client) c).setNom("John");
+		((fr.proxibanque.model.Client) c).setIdClient(1001);
+		Response putResponse = putTarget.request().put(Entity.entity(c, MediaType.APPLICATION_XML));
+
+		System.out.println("put =" + putResponse.getStatus());
+		client.close();
+		response.close();
+
+	}
 }
