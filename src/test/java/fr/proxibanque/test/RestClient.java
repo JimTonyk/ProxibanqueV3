dafe80cd232da9bb613daf8a50ae3965763f14f6
Jean-Michel Hiltbrunner <jm.hiltbrunner@outlook.com>
2018-04-19 13:05:57 +0200

    Modification du WS Client (test en cours)

--- src/test/java/fr/proxibanque/test/RestClient.java
+++ src/test/java/fr/proxibanque/test/RestClient.java
@@ -7,32 +7,30 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import fr.proxibanque.model.Client;
-
 public class RestClient {
 	private static final String PATIENTS = "/Clients";
 	private static final String SERVICE_PATIENTS = "http://localhost:8080/Proxibanque/services/clientservice";
-	
-	
-	public static void main(String[] args) {
-		Client client = ClientBuilder.newClient();
-		// Cible
-		WebTarget target = client.target(SERVICE_PATIENTS).path(PATIENTS).path("/1000");
-		WebTarget putTarget = client.target(SERVICE_PATIENTS).path(PATIENTS);
-		// Verbe
-		Response response = target.request().get();
-		Client p = response.readEntity(Client.class);
-		//Afficher Get
-		System.out.println(p.getNom() + " status " + response.getStatus());
-		
-		// Méthode put
-		Patient patient = new Patient();
-		patient.setName("Avrell");
-		patient.setId(123);
-		Response putResponse = putTarget.request().put(Entity.entity(patient, MediaType.APPLICATION_XML));
-		
-		System.out.println("put ="+putResponse.getStatus());
-		client.close();
-		response.close();
 
+//	public static void main(String[] args) {
+//		Client client = ClientBuilder.newClient();
+//		// Cible
+//		WebTarget target = client.target(SERVICE_PATIENTS).path(PATIENTS).path("/1000");
+//		WebTarget putTarget = client.target(SERVICE_PATIENTS).path(PATIENTS);
+//		// Verbe
+//		Response response = target.request().get();
+//		((fr.proxibanque.model.Client) p) = response.readEntity(Client.class);
+//		// Afficher Get
+//		System.out.println(p.getNom() + " status " + response.getStatus());
+//
+//		// Méthode put
+//		(fr.proxibanque.model.Client) c = new Client();
+//		((fr.proxibanque.model.Client) c).setNom("John");
+//		((fr.proxibanque.model.Client) c).setIdClient(1001);
+//		Response putResponse = putTarget.request().put(Entity.entity(c, MediaType.APPLICATION_XML));
+//
+//		System.out.println("put =" + putResponse.getStatus());
+//		client.close();
+//		response.close();
+//
+//	}
 }
